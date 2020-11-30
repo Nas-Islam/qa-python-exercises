ee23341458745dbd0ac0199254ab0ea56d58f955
Nas-Islam <islam626@hotmail.co.uk>
2020-11-30 13:51:12 +0000

    Added ISBN Exercise answer

---
+++ isbn.py
@@ -0,0 +1,14 @@
+digit = list(str(input("Please enter the first 12 digits of your ISBN code without hyphens: ")))
+digitsum = 0
+
+for i in range(0,len(digit)):
+    if i % 2 == 0:
+        digitsum += int(digit[i])
+    else:
+        digitsum += 3 * int(digit[i])
+
+digit_12 = str(10 - (digitsum % 10))
+digit.append(digit_12)
+ISBNCode = [digit[0], digit[1], digit[2], '-', digit[3], '-', digit[4], digit[5], digit[6], '-', digit[7], digit[8], digit[9], digit[10], digit[11], '-', digit[12]]
+
+print('Your 13 digit ISBN code is: ',''.join(ISBNCode))
