fb6f0dfcf23f2411aa7629d554928861ea013a5d
Nas-Islam <islam626@hotmail.co.uk>
2020-12-09 17:16:07 +0000

    Challenge Day 3 added

---
+++ programs/challenge_day3.py
@@ -0,0 +1,13 @@
+def zip(input1, input2):
+    if len(input1) == len(input2):
+        output = ""
+        for i in range(len(input1)):
+            output += input1[i] + input2[i]
+        return output
+    else:
+        return "Input two Strings of equal length."
+
+print(zip("String", "Fridge"))
+print(zip("Dog", "Cat"))
+print(zip("True", "Tre4"))
+
